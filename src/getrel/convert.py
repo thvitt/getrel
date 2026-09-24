584f288a046ca30a7a65f2bb66e169165d1f4ece
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2026-09-24 11:35:24 +0200

    convert postinstall

--- src/getrel/convert.py
+++ src/getrel/convert.py
@@ -42,6 +42,8 @@ def _make_action(action: str, source: str, arg: str | None = None) -> Action:
                 return BinAction(source=source, link=arg)
             else:
                 return BinAction(source=source, bin=arg)
+        case "postinstall":
+            return ScriptAction(script=arg)
         case _:
             raise ValueError(f"Unknown action {action} for source {source}")
 
